1c4ac823eb7a5fd4ea29148b8bc31306f0b791f4
raphaelmoreno7-creator <raphael.moreno7@icloud.com>
2026-08-24 14:23:10 +0200

    Création de la caisse MargueriteMarket

---
+++ README.md
@@ -0,0 +1,8 @@
+# 🌼 MargueriteMarket — Caisse
+Logiciel de caisse fictif pour jeu de rôle.
+
+Fonctions : codes produits, panier, quantités, total, paiements simulés, espèces et monnaie, ticket de caisse et carte fidélité fictive.
+
+## Carte fidélité
+Au paiement, entre un numéro fictif de 6 à 12 chiffres. Il est affiché sur le ticket sous forme masquée (4 derniers chiffres).
+Aucun paiement réel ni aucune donnée bancaire réelle ne sont utilisés.
---
+++ products.js
@@ -0,0 +1,212 @@
+const PRODUCTS = [
+  {
+    "code": "100001",
+    "name": "Stylo gel rose",
+    "price": 1.5
+  },
+  {
+    "code": "100002",
+    "name": "Stylo décoratif",
+    "price": 2.2
+  },
+  {
+    "code": "100003",
+    "name": "Stylo fantaisie animal",
+    "price": 2.5
+  },
+  {
+    "code": "100004",
+    "name": "Crayon à papier",
+    "price": 0.8
+  },
+  {
+    "code": "100005",
+    "name": "Crayon de couleur",
+    "price": 0.9
+  },
+  {
+    "code": "100006",
+    "name": "Feutre fin",
+    "price": 1.1
+  },
+  {
+    "code": "100007",
+    "name": "Feutre couleur",
+    "price": 1.3
+  },
+  {
+    "code": "100008",
+    "name": "Lot de feutres",
+    "price": 4.9
+  },
+  {
+    "code": "100009",
+    "name": "Boîte de crayons de couleur",
+    "price": 5.9
+  },
+  {
+    "code": "100010",
+    "name": "Surligneur",
+    "price": 1.4
+  },
+  {
+    "code": "100011",
+    "name": "Lot de surligneurs",
+    "price": 5.5
+  },
+  {
+    "code": "100012",
+    "name": "Lot de petits stylos multicolores",
+    "price": 4.9
+  },
+  {
+    "code": "100013",
+    "name": "Lot de crayons multicolores",
+    "price": 5.9
+  },
+  {
+    "code": "100014",
+    "name": "Lot de marqueurs",
+    "price": 6.9
+  },
+  {
+    "code": "100015",
+    "name": "Gomme blanche",
+    "price": 0.8
+  },
+  {
+    "code": "100016",
+    "name": "Gomme fantaisie",
+    "price": 1.5
+  },
+  {
+    "code": "100017",
+    "name": "Lot de gommes fantaisie",
+    "price": 3.5
+  },
+  {
+    "code": "100018",
+    "name": "Taille-crayon simple",
+    "price": 1.2
+  },
+  {
+    "code": "100019",
+    "name": "Taille-crayon double",
+    "price": 1.8
+  },
+  {
+    "code": "100020",
+    "name": "Petit carnet",
+    "price": 2.9
+  },
+  {
+    "code": "100021",
+    "name": "Carnet décoratif",
+    "price": 3.5
+  },
+  {
+    "code": "100022",
+    "name": "Bloc-notes décoratif",
+    "price": 3.9
+  },
+  {
+    "code": "100023",
+    "name": "Petit cahier",
+    "price": 3.5
+  },
+  {
+    "code": "100024",
+    "name": "Cahier / carnet à motifs",
+    "price": 4.5
+  },
+  {
+    "code": "100025",
+    "name": "Cahier d'activités",
+    "price": 5.9
+  },
+  {
+    "code": "100026",
+    "name": "Cahier de coloriage",
+    "price": 5.9
+  },
+  {
+    "code": "100027",
+    "name": "Pot à crayons simple",
+    "price": 5.9
+  },
+  {
+    "code": "100028",
+    "name": "Pot à crayons décoratif",
+    "price": 8.9
+  },
+  {
+    "code": "100029",
+    "name": "Boîte de rangement transparente",
+    "price": 5.9
+  },
+  {
+    "code": "100030",
+    "name": "Trousse rose",
+    "price": 7.9
+  },
+  {
+    "code": "100031",
+    "name": "Trousse fantaisie",
+    "price": 8.9
+  },
+  {
+    "code": "100032",
+    "name": "Petite boîte à accessoires",
+    "price": 4.9
+  },
+  {
+    "code": "100033",
+    "name": "Sachet de perles colorées",
+    "price": 3.9
+  },
+  {
+    "code": "100034",
+    "name": "Sachet de perles lettres",
+    "price": 4.5
+  },
+  {
+    "code": "100035",
+    "name": "Boîte de perles / accessoires",
+    "price": 4.9
+  },
+  {
+    "code": "100036",
+    "name": "Lot d'accessoires créatifs",
+    "price": 3.9
+  },
+  {
+    "code": "100037",
+    "name": "Porte-clés peluche",
+    "price": 4.9
+  },
+  {
+    "code": "100038",
+    "name": "Porte-clés fantaisie",
+    "price": 3.9
+  },
+  {
+    "code": "100039",
+    "name": "Gourde / bouteille décorative",
+    "price": 9.9
+  },
+  {
+    "code": "100040",
+    "name": "Gobelet décoratif",
+    "price": 6.9
+  },
+  {
+    "code": "100041",
+    "name": "Petite boîte décorative",
+    "price": 3.9
+  },
+  {
+    "code": "100042",
+    "name": "Accessoire fantaisie divers",
+    "price": 2.9
+  }
+];
